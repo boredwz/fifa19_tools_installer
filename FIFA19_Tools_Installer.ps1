6368e40270106ce06d5240f78ee5ae0c7892131f
wvzxn <zvp.ukr01@gmail.com>
2023-05-15 23:17:34 +0300

    Big update - backup fixed - fifa19key added - webclient edit - launcher.vbs work dir fix - shortcut and key options added - code improvements

--- FIFA19_Tools_Installer.ps1
+++ FIFA19_Tools_Installer.ps1
@@ -4,9 +4,9 @@
 #    wvzxn | https://github.com/wvzxn/
 #  
 #  [Credits]
-#    Get-FileFromWeb.ps1 | ChrisStro | https://gist.github.com/ChrisStro/37444dd012f79592080bd46223e27adc
 #    ExtractArchive.ps1  | wvzxn     | https://gist.github.com/wvzxn/8f326deb99c3267ecf741a21fa73becb
 #    Frosty Mod Manager  | CadeEvs   | https://github.com/CadeEvs/FrostyToolsuite
+#    FIFA19key           | fifermods | https://www.fifermods.com/frosty-key
 #    Extreme Injector    | master131 | https://github.com/master131/ExtremeInjector
 #  
 #  [Description]
@@ -23,22 +23,27 @@ function Uninstall
         "$env:USERPROFILE\Desktop\FIFA 19 Launcher.lnk" ,
         "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\FIFA 19 Launcher.lnk"
     ) | ?{ Test-Path $_ } | Remove-Item
-    
-    "FrostyModManager\Mods" | ?{ Test-Path $_ } | Copy-Item -Destination "#backup_Mods" -Recurse
+
     "$env:LOCALAPPDATA\Frosty" | ?{ Test-Path $_ } | Remove-Item -Recurse
     "ModData" | ?{ Test-Path $_ } | Remove-Item -Recurse
-    "FrostyModManager" | ?{ Test-Path $_ } | Remove-Item -Recurse
-    mkdir "FrostyModManager" | Out-Null
-    "#backup_Mods" | ?{ Test-Path $_ } | Move-Item -Destination "FrostyModManager\$_" -Force
+    if (Test-Path "FrostyModManager")
+    {
+        Get-ChildItem "FrostyModManager" -Exclude "Mods","#backup_Mods" | Remove-Item -Recurse
+        "FrostyModManager\Mods\FIFA19" | ?{ Test-Path $_ } | ForEach-Object
+        {
+            mkdir "#backup_Mods" | Out-Null
+            Move-Item $_ -Destination "#backup_Mods" -Recurse -Force
+        }
+    }
+    else { mkdir "FrostyModManager" | Out-Null }
 }
 
 #   Import
-$urlWC = "https://gist.githubusercontent.com/ChrisStro/37444dd012f79592080bd46223e27adc/raw/5ba566bd030b89358ba5295c04b8ef1062ddd0ce/Get-FileFromWeb.ps1"
 $urlExtract = "https://gist.githubusercontent.com/wvzxn/8f326deb99c3267ecf741a21fa73becb/raw/ebb82b5257d49f4e35869ff513aa6412a375ed81/ExtractArchive.ps1"
 $urlFrosty = "https://github.com/CadeEvs/FrostyToolsuite/releases/latest/download/FrostyModManager.zip"
+$urlFrostyKey = "https://raw.githubusercontent.com/wvzxn/fifa19_tools_installer/master/frosty_key_fifa19"
 $urlInjector = "https://github.com/master131/ExtremeInjector/releases/download/v3.7.3/Extreme.Injector.v3.7.3.-.by.master131.rar"
-Invoke-WebRequest -useb "$urlWC" | Invoke-Expression          # Get-FileFromWeb $Url  $Path
-Invoke-WebRequest -useb "$urlExtract" | Invoke-Expression     # ExtractArchive  $Path $Destination
+Invoke-WebRequest -useb "$urlExtract" | Invoke-Expression
 
 if ((Get-Location).Path -notmatch "FIFA") { Write-Warning "Run this script inside FIFA 19 folder"; return }
 
@@ -50,9 +55,12 @@ Uninstall
 if ($Uninstall) { Write-Host "Uninstalling completed."; return }
 
 #   Download Tools
-Get-FileFromWeb "$urlFrosty" "fmm.zip"
-Get-FileFromWeb "$urlInjector" "ei.rar"
+Write-Host "== Downloading Frosty Mod Manager... [", ("{0:N2}" -f ((Invoke-WebRequest $urlFrosty -Method Head).Headers.'Content-Length' / 1MB)), "MB ]"
+(New-Object System.Net.WebClient).DownloadFile($urlFrosty, (Get-Location).Path + "\fmm.zip")
+Write-Host "== Downloading Extreme Injector... [", ("{0:N2}" -f ((Invoke-WebRequest $urlInjector -Method Head).Headers.'Content-Length' / 1MB)), "MB ]"
+(New-Object System.Net.WebClient).DownloadFile($urlInjector, (Get-Location).Path + "\ei.rar")
 ExtractArchive @("fmm.zip", "ei.rar") @("", "FrostyModManager")
+Remove-Item @("fmm.zip", "ei.rar")
 
 #   Create FIFA19_Launcher.vbs
 @(
@@ -65,16 +73,20 @@ ExtractArchive @("fmm.zip", "ei.rar") @("", "FrostyModManager")
     "",
     "'   Run as Administrator",
     "Set UAC = CreateObject(`"Shell.Application`")",
-    "UAC.ShellExecute inj, `"`", `"`", `"runas`", 1",
-    "UAC.ShellExecute fmm, `"`", `"`", `"runas`", 1"
+    "UAC.ShellExecute inj, `"`", scriptDir, `"runas`", 1",
+    "UAC.ShellExecute fmm, `"`", scriptDir, `"runas`", 1"
 ) | Set-Content "FrostyModManager\#FIFA19_Launcher.vbs"
 
 #   Create FIFA19_Launcher Shortcut
-$Shortcut = (New-Object -comObject WScript.Shell).CreateShortcut("$env:USERPROFILE\Desktop\FIFA 19 Launcher.lnk")
-$Shortcut.IconLocation = (Resolve-Path "FIFA19.exe").Path
-$Shortcut.TargetPath = (Resolve-Path "FrostyModManager\#FIFA19_Launcher.vbs").Path
-$Shortcut.Save()
-Copy-Item "$env:USERPROFILE\Desktop\FIFA 19 Launcher.lnk" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
+Write-Host "== Press [Y] to add FIFA 19 Launcher shortcuts to Desktop and Start Menu"
+if (([Console]::ReadKey($true).Key) -eq "Y")
+{
+    $Shortcut = (New-Object -comObject WScript.Shell).CreateShortcut("$env:USERPROFILE\Desktop\FIFA 19 Launcher.lnk")
+    $Shortcut.IconLocation = (Resolve-Path "FIFA19.exe").Path
+    $Shortcut.TargetPath = (Resolve-Path "FrostyModManager\#FIFA19_Launcher.vbs").Path
+    $Shortcut.Save()
+    Copy-Item "$env:USERPROFILE\Desktop\FIFA 19 Launcher.lnk" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
+}
 
 #   Bypass Frosty block to create fifa19.cache
 $filter="commentary*.toc"
@@ -83,10 +95,21 @@ foreach ($i in @("data\win32","patch\win32"))
     if (!(Test-Path "$i\comm")) { mkdir "$i\comm" | Out-Null }
     Move-Item "$i\$filter" "$i\comm"
 }
+Write-Warning "Do not close this window."
 Write-Host "== Select FIFA 19 location in Frosty Mod Manager and wait (It may take more than 5 minutes)."
 Write-Host "== Then close Frosty Mod Manager and return to this window."
 Start-Process "FrostyModManager\FrostyModManager.exe" -WorkingDirectory "FrostyModManager" -Verb runAs -Wait
 foreach ($i in @("data\win32","patch\win32")) { Move-Item "$i\comm\$filter" $i }
-Write-Host "== Done!"
+
+#   FIFA19 key
+Write-Host "== You need to enter FIFA 19 KEY to unblock Frosty"
+Write-Host "== Press [Y] to copy key (Frosty will open)"
+if (([Console]::ReadKey($true).Key) -eq "Y")
+{
+    (Invoke-WebRequest -useb $urlFrostyKey).Content | Set-Clipboard
+    Start-Process "FrostyModManager\FrostyModManager.exe" -WorkingDirectory "FrostyModManager" -Verb runAs -Wait
+    Write-Host "== Done!"
+}
+else { Write-Host "== OK, but in case, you can find the key at https://www.fifermods.com/frosty-key" }
 
 return
